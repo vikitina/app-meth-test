0b10a13a4843c579a90d7dfdd99185ee595d6b6a
vikitina <vikitina@gmail.com>
2021-11-12 21:22:38 +0200

    remove log

--- index.ts
+++ index.ts
@@ -3,11 +3,13 @@ import { DependencyScript } from './dependency-script';
 
 let inst = new DependencyScript(list);
 
-console.log(inst.preparedObject);
-
 console.log(`1: ${ inst.getDependenciesList(1) }`);
 console.log(`9: ${ inst.getDependenciesList(9) }`);
-console.log(`--------------All Dependencies----------------------`);
+console.log(`
+
+--------------All Dependencies----------------------
+
+`);
 const allDependenciesLists = inst.getAllDependenciesLists();
 for (let key in allDependenciesLists){
     console.log( `${key} --- ${allDependenciesLists[ key ]}` );
